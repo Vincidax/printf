602dec4c0c17a5a99cffa4ece81ed942d25b1810
SPaci20 <spaci20@gmail.com>
2023-04-23 07:21:22 -0700

    Task 0

---
+++ Task-0.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "main.h"
+
+/**
+ * _printf - A function that produce output according to format
+ * format: specifier
+ *
+ * Return: Number of characters printed
+ */
+
+int _printf(const char *format, ...);
+
